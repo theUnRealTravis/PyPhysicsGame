a0ec914ce69063ac8dd7767a682a8e4478f30abd
erl42000 <erl02040608@gmail.com>
2016-05-21 20:15:07 -0500

    Add files via upload

--- pygametest.py
+++ pygametest.py
@@ -4,44 +4,59 @@ from pygame.locals import *
 
 pygame.init()
 
-FPS = 30
-fpsClock = pygame.time.Clock()
+FPS       = 30
+FPS_CLOCK = pygame.time.Clock()
 
-DISPLAYSURF = pygame.display.set_mode((400, 300), 0, 32)
-pygame.display.set_caption('PYSICS SANDBOX')
+WINDOW_WIDTH  = 400
+WINDOW_HEIGHT = 300
 
-RED   = (255,   0,   0)
-GREEN = (255, 255,   0)
-BLUE  = (  0,   0, 255)
-BLACK = (  0,   0,   0)
-GREY  = (127, 127, 127)
-WHITE = (255, 255, 255)
+WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), 0, 32)
+pygame.display.set_caption('PHYSICS SANDBOX')
+
+RED    = (255,   0,   0)
+YELLOW = (255, 255,   0)
+BLUE   = (  0,   0, 255)
+GREEN  = (255, 255,   0)
+ORANGE = (255, 127,   0)#!
+PURPLE = (127,   0, 127)
+BLACK  = (  0,   0,   0)
+GREY   = (127, 127, 127)
+WHITE  = (255, 255, 255)
+
+OBJECTS_LIST = []
+
+class Brick:
+
+    def __init__(self,x,y,width,height,color):
+        self.change = 1.0
+        self.x      = x
+        self.y      = y
+        self.width  = width
+        self.height = height
+        self.color  = color
+        OBJECTS_LIST.append(self)
+
+    def draw(self):
+        pygame.draw.rect(WINDOW, self.color,(self.x, self.y, self.width, self.height))
+
+    def drop(self):
+        self.y = max(0, min(275, self.y + self.change*2))
+        if self.change < 8:  
+            self.change = self.change * 1.2
 
 def main():
-#over here we will create a menu system where we can select whether we will be in play or construct mode
 
+    brick1 = Brick(200,150,75,25,BLACK)
 
-def play():
-
-    x = 0.0
-    y = 0.0
-    change = 1.0
-    
     while True:
-        DISPLAYSURF.fill(WHITE)
-        pygame.draw.rect(DISPLAYSURF, RED,(x, y, 75, 25))
-        y = max(0, min(275, y + change*2))
-        if change < 8:  
-            change = change * 1.2
+        WINDOW.fill(WHITE)
+        brick1.draw()
+        brick1.drop()
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
         pygame.display.update()
-        fpsClock.tick(FPS)
+        FPS_CLOCK.tick(FPS)
         
-#play()
-
-def construct():
-
-#construct():
+main()
