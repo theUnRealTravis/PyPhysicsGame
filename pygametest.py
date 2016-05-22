2ec56bf6e14c4c69fae8ea7b1e5b6172d93ae2d7
erl42000 <erl02040608@gmail.com>
2016-05-22 02:18:55 -0500

    Add files via upload

--- pygametest.py
+++ pygametest.py
@@ -17,7 +17,7 @@ RED    = (255,   0,   0)
 YELLOW = (255, 255,   0)
 BLUE   = (  0,   0, 255)
 GREEN  = (255, 255,   0)
-ORANGE = (255, 127,   0)#!
+ORANGE = (255, 127,   0)
 PURPLE = (127,   0, 127)
 BLACK  = (  0,   0,   0)
 GREY   = (127, 127, 127)
@@ -28,30 +28,40 @@ OBJECTS_LIST = []
 class Brick:
 
     def __init__(self,x,y,width,height,color):
-        self.change = 1.0
-        self.x      = x
-        self.y      = y
-        self.width  = width
-        self.height = height
-        self.color  = color
+        self.collidedBrick = None
+        self.collide = False
+        self.change  = 1.0
+        self.color   = color
+        self.rect    = pygame.Rect(x,y,width,height)
         OBJECTS_LIST.append(self)
-
+        
     def draw(self):
-        pygame.draw.rect(WINDOW, self.color,(self.x, self.y, self.width, self.height))
+        pygame.draw.rect(WINDOW, self.color,self.rect)
 
     def drop(self):
-        self.y = max(0, min(275, self.y + self.change*2))
-        if self.change < 8:  
-            self.change = self.change * 1.2
+        for i in OBJECTS_LIST:
+            if self.rect.colliderect(i.rect) == 1 and self != i:
+                self.collide = True
+                self.collidedBrick = i
+        if not self.collide:
+            self.rect.y = max(0, min(275, self.rect.y + self.change*2))
+            if self.change < 8:  
+                self.change = self.change * 1.2
+        else:
+            if self.collidedBrick.rect.top > self.rect.top:
+                self.rect.bottom = self.collidedBrick.rect.top
 
 def main():
 
-    brick1 = Brick(200,150,75,25,BLACK)
+    brick1 = Brick(200,100,75,25,RED)
+    brick2 = Brick(130,50,75,25,BLUE)
 
     while True:
         WINDOW.fill(WHITE)
         brick1.draw()
         brick1.drop()
+        brick2.draw()
+        brick2.drop()
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
